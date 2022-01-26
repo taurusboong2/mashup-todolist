d6e8d73eb4178cb5c4c12aa7680b3921473bbcf3
taurusboong2 <taurusboong2@gmail.com>
2022-01-26 20:57:09 +0900

    create components

---
+++ src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px;
+  padding-bottom: 48px;
+  overflow-y: auto;
+`;
+
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="리액트 배우기" done={true} />
+      <TodoItem text="컴포넌트 생성하기" done={true} />
+      <TodoItem text="리액트 복습하기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>
+  );
+}
+
+export default TodoList;
